92d84f34b2dd730fec8d554988f975a171565662
jessicafarias <jessica.farias.rosado@gmail.com>
2020-09-10 12:59:36 -0500

    Adding helper in events_helper
    Trying to add a new attendance in show event

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,6 @@
 module EventsHelper
+  def prueba(event)
+      event.attendances.build(user_id: current_user.id)
+      event.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,4 @@ class User < ApplicationRecord
   
   has_many :attendances
   has_many :attended_events, through: :attendances, source: :event
-
-
 end
