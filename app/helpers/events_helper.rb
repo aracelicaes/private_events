df3549c34f7f74b9b2ead1b9e538dac76a03acc8
jessicafarias <jessica.farias.rosado@gmail.com>
2020-09-11 16:15:05 -0500

    Adding displaylist Helper
    Update event#index view
    Update event#show view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,21 @@
 module EventsHelper
+  def displaylist(list)
+    var = content_tag(:div) do
+    end
+    
+    list.each do |event|
+    var +=
+      content_tag(:div, class: 'w-100 col-3') do
+        content_tag(:div) do
+          link_to image_tag('event.jpeg', alt: "Logo", class: "w-100"), event
+        end +
+        content_tag(:p) do
+          "#{event.title} #{event.date}"
+        end 
+      end
+    end
+
+    var
+  end
+
 end
