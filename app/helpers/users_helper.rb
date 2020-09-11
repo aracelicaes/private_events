1a2ca3fa797f6d336cd3116fcc3941d0468d6e7c
jessicafarias <jessica.farias.rosado@gmail.com>
2020-09-11 16:29:39 -0500

    Adding displaylist_user helper to display users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  def displaylist_user(list)
+    var = content_tag(:div) do
+    end
+    
+    list.each do |user|
+    var +=
+      content_tag(:div, class: 'w-100 col-2') do
+        content_tag(:div) do
+          link_to image_tag('woman.jpg', alt: "Logo", class: "w-100"), user
+        end +
+        content_tag(:p) do
+          "#{user.username}"
+        end 
+      end
+    end
+
+    var
+  end
 end
