a0eec0b768b7b21e100354d692bcd12b9b15ab49
aracelicaes <aracelicaes@gmail.com>
2020-09-15 12:15:01 -0500

    Add Validations for Events rspec

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 10 }, uniqueness: true
+  validates :date, presence: true
+  validates :user_id, presence: true
+
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
 
   has_many :attendances
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  
+
+  describe 'Validate' do
+    let (:user) {User.new(name: 'name', username: 'username', id: 3)}
+    let (:event_1) {Event.new(title: 'Independencia')}
+    
+    it 'Invalid Event due to nil data' do
+      expect(event_1).to_not be_valid
+    end
+
+    it 'Valid Event with all attributes' do
+      expect(Event.new(title: 'Independencia', date: Date.tomorrow, creator: user)).to be_valid
+    end
+  end
 end
