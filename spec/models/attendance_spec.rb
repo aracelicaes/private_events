45aaf9ec0e7955cb46b26ef93861715c2bf1e9b8
aracelicaes <aracelicaes@gmail.com>
2020-09-15 15:19:09 -0500

    Adding Attendance associations rspec

---
+++ spec/models/attendance_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Attendance, type: :model do
+
+  # Validates 2 foreign keys(user_id, event_id) and 2 belongs_to (:user, :event)
+  context "#Associations" do
+    it 'Attendance has a foreign_key from user_id' do
+      association = Attendance.reflect_on_association(:user)
+      expect(association.foreign_key).to eq("user_id")
+    end
+
+    it 'Attendance has foreign_key from event_id' do
+      association = Attendance.reflect_on_association(:event)
+      expect(association.foreign_key).to eq("event_id")
+    end
+
+    it 'Attendance belongs to User Class' do
+      association = Attendance.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it 'Attendance belongs to Event Class' do
+      association = Attendance.reflect_on_association(:event)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+end
